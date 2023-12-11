43eb1279e2886956e0996b88606937d54f7ac47e
Ismael Hallal <ismael.h.lamela@gmail.com>
2023-12-11 18:35:14 +0100

    part2: Exercise 2

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -32,6 +32,7 @@ const Course = (props) => {
     <div>
       <Header courseName={name} />
       <Content parts={parts} />
+      <Total sumOfExercises={total} />
     </div>
   )
 }
@@ -43,6 +44,7 @@ const App = () => {
       { id: 1, name: 'Fundamentals of React', exercises: 10 },
       { id: 2, name: 'Using props to pass data', exercises: 7 },
       { id: 3, name: 'State of a component', exercises: 14 },
+      { id: 4, name: 'Redux', exercises: 11 }
     ]
   }
 
