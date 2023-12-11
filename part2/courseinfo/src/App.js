bba953897c168a4bea4ed70a73c71c125ab6c9fb
Ismael Hallal <ismael.h.lamela@gmail.com>
2023-12-11 18:34:36 +0100

    part2: Exercise 1

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,56 @@
+const Header = (props) => {
+  return <h1>{props.courseName}</h1>
+}
+
+const Total = (props) => {
+  return <p><strong>Number of exercises {props.sumOfExercises}</strong></p>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  )
+}
+
+const Course = (props) => {
+  const { name, parts } = props.course
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <div>
+      <Header courseName={name} />
+      <Content parts={parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { id: 1, name: 'Fundamentals of React', exercises: 10 },
+      { id: 2, name: 'Using props to pass data', exercises: 7 },
+      { id: 3, name: 'State of a component', exercises: 14 },
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
