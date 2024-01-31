f336af9842dd6aa00a946096136be0d7edd33d5a
Ismael <ismael.h.lamela@gmail.com>
2024-01-31 14:39:54 +0100

    part2: Exercise 17

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -19,6 +18,8 @@ const App = () => {
   }
 
   useEffect(hook, [])
+
+  useEffect(hook, [persons])
 
   const addPerson = (event) => {
     event.preventDefault()
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import PersonService from '../services/PersonService';
+
+function DeleteUser(id, user) {
+  let msg = "¿Estás seguro de que quieres borrar a " + user + "?";
+  if (window.confirm(msg)) {
+    alert(user + " fue eliminado");
+    PersonService.remove(id)
+    .then(response => console.log(response))
+  } else {
+    alert(user + " no fue eliminado de la app");
+  }
+}
 
 const Persons = ({ personsToShow }) => {
   return (
     <div>
-      {personsToShow.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {personsToShow.map((person, index) => (
+        <div key={index}>
+          <p style={{ display: 'inline-block', marginRight: '8px' }}>{person.name} {person.number}</p>
+          <button onClick={() => DeleteUser(person.id, person.name)}>Delete</button>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Persons;
